58cfe6c9896d49d1a78eb1c77cc6391f4723c5b6
Paweł Iwaneczko <p.iwaneczko@aircom.ag>
2018-12-10 21:08:56 +0100

    Implemented algorithm for day 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,36 @@
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <string>
 
 using namespace std;
-int main(int argc, char *argv[]) {
-    size_t sum = 0;
+int main(const int argc, char *argv[]) {
+    size_t twoCount = 0, threeCount = 0;
     if (argc > 1) {
         fstream input(argv[1]);
         if (input.is_open()) {
             while (!input.eof()) {
                 string line;
                 getline(input, line);
-                sum += stoi(line);
+                map<char, size_t> letters;
+                for (const auto &ch : line) {
+                    letters[ch]++;
+                }
+                auto twoCounted = false;
+                auto threeCounted = false;
+                for (const auto &it : letters) {
+                    if (!twoCounted && it.second == 2) {
+                        twoCount++;
+                        twoCounted = true;
+                    } else if (!threeCounted && it.second == 3) {
+                        threeCount++;
+                        threeCounted = true;
+                    }
+                }
             }
         }
     }
-    cout << "Answer:" << sum << endl;
+    cout << "Answer:" << twoCount << " * " << threeCount << " = " << twoCount * threeCount << endl;
     system("pause");
     return 0;
 }
